ac929a231b2ca55c9f62e8e12de04e66bea516ee
Evan Krambuhl <thekrambuhl@gmail.com>
2016-05-07 03:28:31 -0700

    super cool go to bed

--- source/Main.elm
+++ source/Main.elm
@@ -2,47 +2,101 @@ module Main where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy, lazy2, lazy3)
+
+import Signal exposing (Signal, Address, send)
 import Json.Encode exposing (string)
 
 
-type Action
-    = NoOp
-    | AddHistory
-    | RemoveHistory
-    | EditHistory
-
+-- DATA TYPES
 
 type alias Model = 
-    { events: List Event
-    , cursor: String }
+  { events: List Event
+  , cursor: String }
 
 
 type alias Event = 
-    { uid: Int
-    , title: String
-    , href: String }
+  { uid: Int
+  , title: String }
 
 
-updateEvent : Action -> Event -> Event
-updateEvent action event = 
-    case action of
-        NoOp -> event
-        AddHistory -> 
-            { event | 
-                uid = event.uid + 1 }   
-        RemoveHistory -> event
-        EditHistory -> event
+emptyModel : Model
+emptyModel =
+  { events = []
+  , cursor = "" }
+
+newEvent: Int -> String -> Event
+newEvent id name =
+  { uid = id
+  , title = name }
 
 
-questionView : String -> Html
-questionView question = 
-    div [ class "question"
-        , attribute "aria-role" "question" ]
-        [ span 
-            [ ]
-            [ text question ]]
+--[ eventView "What's the history around here?" 
+--, eventView "Tell a story." ]
 
+-- UPDATES
+
+type Action
+  = NoOp
+  | AddEvent Int String
+  | RemoveEvent Int
+  | EditEvent
+
+
+update : Action -> Model -> Model
+update action model = 
+  case action of
+    NoOp -> model
+    
+    AddEvent id title ->
+      { model | events = model.events ++ [newEvent id title] }
+    
+    RemoveEvent id -> 
+      { model | events = List.filter (\ev -> ev.uid /= id) model.events}
+    
+    EditEvent -> model
+
+
+
+-- VIEWS
+
+eventView : Address Action -> Event -> Html
+eventView address events = 
+  div 
+    [ class "question" 
+    , onClick address (RemoveEvent events.uid) ]
+    [ span 
+      [ ]
+      [ text (toString events.uid)
+      , text ". "
+      , text events.title ] ]
+
+
+-- MAIN
+
+view : Address Action -> Model -> Html
+view address model =
+  div 
+    [ class "app" ]  
+    [ div []
+      [ h1 
+        [ onClick address (AddEvent (List.length model.events) "Tell a story.") ]
+        [ text "Click for a story" ] ]
+    , div [] 
+      (List.map (eventView address) model.events)]
+
+
+model : Signal Model
+model =
+  Signal.foldp update emptyModel actions.signal
+
+
+main : Signal Html
 main = 
-    div [ class "app" ] 
-        [ questionView "What's the history around here?" 
-        , questionView "Tell a story." ]
+    Signal.map (view actions.address) model
+
+
+actions : Signal.Mailbox Action
+actions =
+  Signal.mailbox NoOp
