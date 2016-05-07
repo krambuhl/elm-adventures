4aa65ce4cad801eccc454f1c29264c672cd44eba
Evan Krambuhl <thekrambuhl@gmail.com>
2016-05-07 00:39:45 -0700

    elm is exciting!

--- source/Main.elm
+++ source/Main.elm
@@ -2,9 +2,47 @@ module Main where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Json.Encode exposing (string)
 
-import Components.Hecker exposing (..)
+
+type Action
+    = NoOp
+    | AddHistory
+    | RemoveHistory
+    | EditHistory
+
+
+type alias Model = 
+    { events: List Event
+    , cursor: String }
+
+
+type alias Event = 
+    { uid: Int
+    , title: String
+    , href: String }
+
+
+updateEvent : Action -> Event -> Event
+updateEvent action event = 
+    case action of
+        NoOp -> event
+        AddHistory -> 
+            { event | 
+                uid = event.uid + 1 }   
+        RemoveHistory -> event
+        EditHistory -> event
+
+
+questionView : String -> Html
+questionView question = 
+    div [ class "question"
+        , attribute "aria-role" "question" ]
+        [ span 
+            [ ]
+            [ text question ]]
 
 main = 
     div [ class "app" ] 
-        [ hecker, hecker, hecker ]
+        [ questionView "What's the history around here?" 
+        , questionView "Tell a story." ]
--- source/components/Hecker.elm
+++
@@ -1,9 +0,0 @@
-module Components.Hecker (..) where
-
-import Html exposing (..)
-import Html.Attributes exposing (..)
-
-hecker : Html
-hecker = 
-  div [ class "hecker" ] [ text "what a hecker." ]
-
